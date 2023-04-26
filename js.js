8546acdbd9a75b7afa6e96c00064fb691381a40d
Nihilicious <nihilidom@gmail.com>
2023-04-26 17:36:34 +0300

    reworked box generation and added a functional hover event listener

--- js.js
+++ js.js
@@ -1,12 +1,23 @@
-const canvas = document.querySelector('.canvas');
-let htmlstring = '';
-for (let i = 1; i <= 16; i++) { 
-    for (let o = 1; o <= 16; o++){
-        htmlstring += `<div class="box"></div>`;
-    };
+const container = document.querySelector('.container');
+
+const canvas = document.createElement('div');
+canvas.classList.add('canvas')
+
+let choice = 16;
+let count = choice*choice;
+const boxes = [];
+
+for(let i = 1; i<=count; i++){
+    boxes.push(i);
+    boxes[i-1] = document.createElement('div');
+    boxes[i-1].classList.add('box');
+    boxes[i-1].addEventListener('mouseenter', function(){
+        boxes[i-1].classList.add('hovered');
+    });
+    canvas.appendChild(boxes[i-1]);
 };
 
 
-
-
-canvas.innerHTML = htmlstring;
+console.log(boxes);
+console.log(canvas);
+container.appendChild(canvas);
