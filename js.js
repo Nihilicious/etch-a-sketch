6743d6da1212be03e6d8b9214c40b8a3cbbaedd8
Nihilicious <nihilidom@gmail.com>
2023-05-03 13:17:22 +0300

    make grid items change size with grid density

--- js.js
+++ js.js
@@ -15,7 +15,9 @@ function createCanvas(sizin){
         boxes.push(i);
         boxes[i-1] = document.createElement('div');
         boxes[i-1].classList.add('box');
-        boxes[i-1].addEventListener('mouseenter', function(){
+        boxes[i-1].style.height = `${box_dimensions}px`;
+        boxes[i-1].style.width = `${box_dimensions}px`;
+        boxes[i-1].addEventListener('mouseover', function(){
             boxes[i-1].classList.add('hovered');
         });
         canvas.appendChild(boxes[i-1]);
@@ -27,10 +29,12 @@ function createCanvas(sizin){
 let choice = 16;
 let count = choice*choice;
 let boxes = [];
+let box_dimensions = 0;
 
 button.addEventListener('click', function(){
     choice = prompt('choose your grid size (up to 100)', 16);
     count = choice*choice;
+    box_dimensions = 800/choice;
     createCanvas(count);
 });
 
